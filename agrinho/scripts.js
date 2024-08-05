16bca8d93cbc02459bc69b0a9c137dcee64ae6d0
mary1812008 <164516580+mary1812008@users.noreply.github.com>
2024-08-05 20:20:53 -0300

    Projeto Agrinho
    Uso de html, css e se não se importar eu usei um pouco do javascript para ficar algo mais moderno

---
+++ agrinho/scripts.js
@@ -0,0 +1,13 @@
+// Código JavaScript para interatividade (exemplo básico)
+document.addEventListener('DOMContentLoaded', () => {
+    // Adicione código JavaScript aqui, se necessário
+});
+
+
+
+
+
+
+
+
+
